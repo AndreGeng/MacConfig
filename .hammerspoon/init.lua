fe2bed22ac3963f087d2833d98db507c28d10c29
GengAndre <gengze@meituan.com>
2017-02-10 23:51:48 +0800

    hammerspoon & karabiner backup

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,6 @@
+local autoReloadConfig = require('autoReloadConfig')
+local inicatorText = require('modalIndicator')
+local arrowRemapping = require('remapping')
+
+local windowMovement = require('hyperkey-windowAndTabs')
+local launchApp = require('hyperkey-launchapp')
