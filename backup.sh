b2096193a3f3d5abf26ebacc520896438600b26f
gengze <gengze@meituan.com>
2018-05-09 06:34:14 +0800

    remove package.json

---
+++ backup.sh
@@ -0,0 +1,4 @@
+cp -R ~/.hammerspoon .
+cp -R ~/.config/karabiner .
+cp -R ~/.zshrc .
+cp -R ~/.config/nvim .
---
+++ sync.sh
@@ -0,0 +1,8 @@
+rm -rf ~/.hammerspoon
+rm -rf ~/config/karabiner
+rm ~/.zshrc
+rm -rf ~/.config/nvim
+cp -R ./.hammerspoon ~
+cp -R ./karabiner ~/.config
+cp -R ~/.zshrc ~
+cp -R ~/nvim ~/.config
